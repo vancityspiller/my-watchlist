d140bf0467f5906ef193a414603ad50d4aa66ad7
Spiller <xiwcoc@gmail.com>
2022-05-07 13:11:28 +0530

    Put a title

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,8 @@ export default function App() {
     // ------------------------------------------------------- //
 
     return (
-        <React.Fragment />
+        <div className='main'>
+            <span className='title'>my watchlist</span>
+        </div>
     )
 }
